98a7d68ba7a2dbd2e119714543c05101a5a8ecf9
prashant <prashantbachhamadi@gmail.com>
2024-09-20 10:21:06 -0700

    Add files via upload

---
+++ radha.py
@@ -0,0 +1,12 @@
+import webbrowser as wb
+
+webwapp = "https://web.whatsapp.com/"
+chatgpt = "https://chat.openai.com/c/aee35e24-fa6e-4f9c-ae17-49e7daecb2f3"
+yt = "https://www.youtube.com/"
+
+
+a=wb.open
+# Open WhatsApp Web in the default web browser
+a(webwapp)
+a(chatgpt)
+a(yt)
